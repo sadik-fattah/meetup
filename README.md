a8ef74981efc4855de5cf7e0a494f77199523b7a
Vikrant <vikrantverma945@gmail.com>
2020-04-24 11:52:35 +0530

    basic server and file setup

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # meetup
 The Video chat App based on  WEBRTC
+
+Initialize the Node_modules with : npm i
---
+++ server.js
@@ -0,0 +1,19 @@
+const EXPRESS = require("express");
+const app = new EXPRESS();
+
+/*
+Start the server on the specified port
+*/
+var server = app.listen(8080, function() {
+    var host = server.address().address
+    var port = server.address().port
+    console.log("Server Running at  http://%s:%s", host, port)
+})
+
+
+/*
+Send the landing html page to user 
+*/
+app.get("/", (req, res, next) => {
+    res.sendFile(__dirname + "/index.html");
+});
